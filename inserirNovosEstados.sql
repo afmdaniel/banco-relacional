79439f98bb18bc7bf958c68da337e936a0b59149
afmdanielwin <danielfarias380@gmail.com>
2020-05-10 18:58:50 -0300

    Insert with id

--- inserirNovosEstados.sql
+++ inserirNovosEstados.sql
@@ -3,3 +3,11 @@ VALUES (1000, 'Novo', 'NV', 'Sul', 2.54);
 
 INSERT INTO estados (nome, sigla, regiao, populacao)
 VALUES ('Mais Novo', 'MN', 'Nordeste', 2.51);
+
+INSERT INTO estados (id, nome, sigla, regiao, populacao)
+VALUES (999, 'Mais Novo Ainda', 'NN', 'Sudeste', 12.54);
+
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES ('Outro Novo', 'ON', 'Norte', 5.21);
+
+SELECT id, nome FROM estados ORDER BY id;
